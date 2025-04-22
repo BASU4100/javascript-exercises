893cf31b5aad5c25e55871eee3cf62e373a0d8d9
Vivek Kumar <vk13114416@gmail.com>
2025-04-22 04:59:02 +0000

    Completed the palindromes function with correct code to pass all the jest Test.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (stringValue) {
+    stringValue = stringValue.replace(/[^a-z0-9]/gi,"").toLowerCase();
+    let l = 0;
+    let u = stringValue.length-1;
+    console.log(stringValue);
+    while (l<u) {
+        if (stringValue[l++] === stringValue[u--])
+            continue;
+        else
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
