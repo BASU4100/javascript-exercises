34a54a86bf411a2ce7cd69613199209e2b174dd9
Vivek Kumar <vk13114416@gmail.com>
2025-04-22 06:10:23 +0000

    Completed the assignment by using the reduce function to get the oldest person and also used the date object to get the missing dates.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((prevPerson, currPerson) => {
+        return ((age(prevPerson) > age(currPerson))?prevPerson: currPerson);
+    });
 };
+
+let age = (person) => {
+    if (person.yearOfDeath === undefined) {
+        let d = new Date();
+        person.yearOfDeath = d.getFullYear();
+    }
+    return (person.yearOfDeath - person.yearOfBirth)
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
