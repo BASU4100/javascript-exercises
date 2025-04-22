3cb8396223d283fed273fdd6bbbf477ae223979a
Vivek Kumar <vk13114416@gmail.com>
2025-04-22 05:35:31 +0000

    Completed the assignment by filling the code in the function and passed all the jest testcases.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num<0)
+        return "OOPS"
+    let arr = [0, 1];
+    let len = arr.length;
+    if (+num+1<=len)
+        return arr[+num];
+    while (len!=+num+1) {
+        len = arr.push(arr[len-1]+arr[len-2]);
+    }
+    return arr[len-1];
 };
 
 // Do not edit below this line
